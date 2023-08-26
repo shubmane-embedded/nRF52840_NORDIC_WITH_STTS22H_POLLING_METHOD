c548e5da6730be815f7c2c6aa1cb916fe993040f
shubmane-embedded <shubham_embedded@outlook.com>
2023-08-27 00:15:27 +0530

    Initial_commit

---
+++ twi_sensor_STTS22H/pca10056/blank/ses/Output/Release/Obj/twi_sensor_pca10056/ses_nrf52840_Vectors.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/twi_sensor_pca10056/ses_nrf52840_Vectors.o: \
+ /home/new/nrf_sdk/DeviceDownload/nRF5SDK1500a53641a/nRF5_SDK_15.0.0_a53641a/modules/nrfx/mdk/ses_nrf52840_Vectors.s
---
+++ twi_sensor_STTS22H/pca10056/blank/ses/Output/Release/Obj/twi_sensor_pca10056/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/twi_sensor_pca10056/thumb_crt0.o: \
+ /usr/share/segger_embedded_studio_for_arm_5.60a/source/thumb_crt0.s
